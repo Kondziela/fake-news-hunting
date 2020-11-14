a6a73b2741cc9eb1db18d5fa0ef8de9627f4fbce
akondziela <artur.kondziela@gmail.com>
2020-11-14 16:35:18 +0100

    Add request to FakeHunter

---
+++ src/models/models.ts
@@ -0,0 +1,35 @@
+export interface FakeHunterResponse {
+    "total": number;
+    "current_page": number;
+    "page_size": number;
+    "results": Array<{
+        "date": Date;
+        "domains": Array<{
+            "created_at": Date;
+            "id": string;
+            "name": string;
+        }>
+        "expert_opinion": {
+            "title": string,
+            "confirmation_sources": string;
+            "comment": string;
+            "date": Date;
+            "verdict": boolean;
+        },
+        "fact_checker_opinions": Array<any>;
+        "id": string;
+        "is_pinned": boolean;
+        "reported_at": Date;
+        "screenshot_url": string;
+        "tags": Array<{
+            "created_at": Date;
+            "id": string;
+            "name": string;
+        }>
+        "text": string;
+        "title": string;
+        "url": string;
+        "verdict": boolean;
+        "verified_by_expert": boolean;
+    }>
+}
